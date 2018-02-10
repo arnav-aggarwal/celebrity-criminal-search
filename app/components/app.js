914658dca40e787d0b474d1d3e46b6398d8a44fd
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-02-10 12:49:08 -0800

    Replace spaces with tabs

--- app/components/app.js
+++ app/components/app.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h4>CelebritySearch</h4>
-                <ul>
-                    <li>#MeToo</li>
-                    <li>Olympic Athletes</li>
-                    <li>Politicians</li>
-                </ul>
-            </div>
-        )        
-    }
+	render() {
+		return (
+			<div>
+				<h4>CelebritySearch</h4>
+				<ul>
+					<li>#MeToo</li>
+					<li>Olympic Athletes</li>
+					<li>Politicians</li>
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
