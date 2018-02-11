3643cb92eeda1690589a8309329abb7335acc06e
Lauren Settle <lauren.settle@thecontrolgroup.com>
2018-02-10 18:05:33 -0800

    build setup and app signing

--- app/index.js
+++ app/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// if(window.FirebasePlugin) {
+// 	window.FirebasePlugin.onNotificationOpen(notification => {
+// 		notification = {
+// 			content: notification.content,
+// 			title: notification.title
+// 		};
+// 		console.log()
+// 	});
+// }
+
 ReactDOM.render(
 	<div>
 		<h4>TCG MOBILE HACKATHON 1.0</h4>
